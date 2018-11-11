9d68955822a31d36704a343391192e2004da3e60
Filipp Shadrin <fil201121@yandex.ru>
2018-11-11 05:11:25 +0500

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/yandexkotlinproject/MainScreenActivity.kt
@@ -0,0 +1,57 @@
+package com.bignerdranch.android.yandexkotlinproject
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
+import android.support.v7.widget.Toolbar
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main_screen.*
+
+class MainScreenActivity : AppCompatActivity() {
+
+    //private lateinit var toolbar: Toolbar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_screen)
+
+        //toolbar = toolbar_main
+        setSupportActionBar(toolbar_main)
+        loadFragment(TranslateFragment(), false)
+        bottom_navigation_main.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+        toolbar_main.title = resources.getString(R.string.translate_icon)
+    }
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener {
+        item ->  when(item.itemId){
+            R.id.translate_navigation -> {
+                //toolbar_main.title = resources.getString(R.string.translate_icon)
+                loadFragment(TranslateFragment(), false)
+                return@OnNavigationItemSelectedListener  true
+            }
+            R.id.bookmarks_history_navigation ->  {
+                toolbar_main.title = resources.getString(R.string.bookmarks_history_navigation)
+                loadFragment(BookmarksHistoryFragment(), false)
+                return@OnNavigationItemSelectedListener  true
+            }
+            R.id.settings_navigation -> {
+                toolbar_main.title = resources.getString(R.string.settings)
+                loadFragment(SettingsFragment(), false)
+                return@OnNavigationItemSelectedListener  true
+            }
+    }
+        Log.d("error", "onNavigationItemSelectedListener упал")
+        return@OnNavigationItemSelectedListener  false
+    }
+
+    private fun loadFragment(fragment: Fragment, isBackStack: Boolean){
+        val fragmentManager = supportFragmentManager.beginTransaction()
+                .replace(R.id.frame_container_main, fragment)
+        when (isBackStack){
+            true -> fragmentManager.addToBackStack(null).commit()
+            false -> fragmentManager.commit()
+        }
+    }
+}
---
+++ app/src/main/java/com/bignerdranch/android/yandexkotlinproject/TranslateFragment.kt
@@ -0,0 +1,34 @@
+package com.bignerdranch.android.yandexkotlinproject
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.activity_main_screen.*
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class TranslateFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_translate, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        activity?.actionBar?.title = "переводчик1"
+    }
+
+
+}

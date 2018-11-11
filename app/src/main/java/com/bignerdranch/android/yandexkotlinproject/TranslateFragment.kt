f7834c3e166030c17e0dfd28ce69c9c0a1032baf
Filipp Shadrin <fil201121@yandex.ru>
2018-11-11 05:20:10 +0500

    First commit

--- app/src/main/java/com/bignerdranch/android/yandexkotlinproject/TranslateFragment.kt
+++ app/src/main/java/com/bignerdranch/android/yandexkotlinproject/TranslateFragment.kt
@@ -27,7 +27,7 @@ class TranslateFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        activity?.actionBar?.title = "переводчик3"
+        activity?.actionBar?.title = "переводчик4"
     }
 
 

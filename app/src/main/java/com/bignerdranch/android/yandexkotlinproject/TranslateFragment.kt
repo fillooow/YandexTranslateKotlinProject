d54be0eead50b9f7f1ad4cccaff9d7078762db04
Filipp Shadrin <fil201121@yandex.ru>
2018-11-16 04:22:12 +0500

    new TranslateFragment style swap languages button - added onClickListener

--- app/src/main/java/com/bignerdranch/android/yandexkotlinproject/TranslateFragment.kt
+++ app/src/main/java/com/bignerdranch/android/yandexkotlinproject/TranslateFragment.kt
@@ -6,7 +6,9 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.activity_main_screen.*
+import android.widget.ImageButton
+import android.widget.TextView
+import kotlinx.android.synthetic.main.fragment_translate.*
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -20,6 +22,10 @@ private const val ARG_PARAM2 = "param2"
  */
 class TranslateFragment : Fragment() {
 
+    lateinit var swapImageButton: ImageButton
+    lateinit var sourceLanguageTV: TextView
+    lateinit var requiredLanguageTV: TextView
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_translate, container, false)
@@ -27,8 +33,26 @@ class TranslateFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        activity?.actionBar?.title = "переводчик5"
+
+        initiateViews()
+
     }
 
+    private fun initiateViews() {
+
+        sourceLanguageTV = source_language
+        requiredLanguageTV = required_language
+
+        swapImageButton = swap_image_button
+        swapImageButton.setOnClickListener {
+            swapLanguages()
+        }
+    }
+
+    private fun swapLanguages(){
+        var tempStr = sourceLanguageTV.text
+        sourceLanguageTV.text = requiredLanguageTV.text
+        requiredLanguageTV.text = tempStr
+    }
 
 }
